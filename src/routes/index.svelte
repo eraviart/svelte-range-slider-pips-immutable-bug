cb6299ba79ca311749acde5b274fae5e3427a7db
Emmanuel Raviart <emmanuel@raviart.com>
2021-03-10 10:37:01 +0100

    Slider working when not using immutable.

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import RangeSlider from "svelte-range-slider-pips";
+
+	let range=[10]
+</script>
+
+<svelte:head>
+	<title>Example of range slider working without immutable option</title>
+</svelte:head>
+
+<RangeSlider bind:values={range} />
